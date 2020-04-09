17f12e2018f0309cedd2a9bfc87e36845fc394fe
dkersten <dkersten1994@gmail.com>
2020-04-09 13:44:56 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -57,8 +57,6 @@ class Student
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
     sql = <<-SQL
       SELECT *
       FROM students
@@ -73,6 +71,7 @@ class Student
 
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
